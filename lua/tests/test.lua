1437cece6e6362fb27f906ac46d1c9fdf6dc2b7e
Kalka <k@kalka.io>
2022-10-22 09:17:53 -0400

    add test lua for json and print table case (pedantic but always good practice)

---
+++ lua/tests/test.lua
@@ -0,0 +1,43 @@
+-- Add path for lua to find the module in ../json/json.lua
+package.path = package.path .. ";../json/?.lua;../?.lua"
+-- Load the module
+local json = require("json")
+local util = require("util")
+
+local alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+local alphanumeric_len = alphanumeric:len()
+
+local function random_string(length)
+    local str = ""
+    for i = 1, length do
+        local index = math.random(1, alphanumeric_len)
+        str = str .. alphanumeric:sub(index, index)
+    end
+    return str
+end
+
+local function random_table(depth, max_depth, max_children)
+    local t = {}
+    local children = math.random(1, max_children)
+    for i = 1, children do
+        local key = random_string(10)
+        local value
+        if depth < max_depth then
+            value = random_table(depth + 1, max_depth, max_children)
+        else
+            value = random_string(10)
+        end
+        t[key] = value
+    end
+    return t
+end
+
+local function test()
+    local t = random_table(1, 5, 10)
+    local json_str = json.encode(t)
+    local decoded = json.decode(json_str)
+    util.pt(decoded)
+    print("Test passed!")
+end
+
+test()
