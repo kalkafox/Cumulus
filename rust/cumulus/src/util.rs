a7d4c67de70244a2e7d75815ce7eeff73bd44684
Kalka <k@kalka.io>
2022-10-24 04:55:12 -0400

    revert these changes

--- rust/cumulus/src/util.rs
+++ rust/cumulus/src/util.rs
@@ -6,15 +6,9 @@ pub fn attach_interrupt_handler(mut function: Option<fn()>) {
 
     if function.is_none() {
         function = Some(|| {
-            // Check if stdin is empty. If it is, print a newline and exit
-            if std::io::stdin().bytes().next().is_none() {
-                logger::info("Exiting...".red().bold().to_string().as_str());
-                // Safely exit the program
-                std::process::exit(0);
-            } else {
-                // Clear the input buffer
-                std::io::stdin().read_line(&mut String::new()).unwrap();
-            }
+            logger::info("Exiting...".red().bold().to_string().as_str());
+            // Safely exit the program
+            std::process::exit(0);
         });
     }
 
