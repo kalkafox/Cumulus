427d11ab7ded0dfd8e7d40651c6d4f44146412f8
Kalka <k@kalka.io>
2022-10-24 04:36:32 -0400

    terminate if the stdin buffer is empty

--- rust/cumulus/src/util.rs
+++ rust/cumulus/src/util.rs
@@ -5,10 +5,15 @@ pub fn attach_interrupt_handler(mut function: Option<fn()>) {
 
     if function.is_none() {
         function = Some(|| {
-            println!();
-            logger::info("Exiting...".red().bold().to_string().as_str());
-            // Safely exit the program
-            std::process::exit(0);
+            // Check if stdin is empty. If it is, print a newline and exit
+            if std::io::stdin().bytes().next().is_none() {
+                logger::info("Exiting...".red().bold().to_string().as_str());
+                // Safely exit the program
+                std::process::exit(0);
+            } else {
+                // Clear the input buffer
+                std::io::stdin().read_line(&mut String::new()).unwrap();
+            }
         });
     }
 
